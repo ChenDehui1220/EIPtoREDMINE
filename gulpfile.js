ec45b9291ce0b0de5c0affdd80bb25168de253ee
gary_chen <gary_chen@hiiir.com>
2017-09-07 17:52:58 +0800

    新增ignore

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,10 @@
 // dependencies
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var minicss = require('gulp-clean-css');
+var minijson = require('gulp-jsonminify');
+var htmlmin = require('gulp-htmlmin');
 var del = require('del');
 var runSequence = require('run-sequence');
 
@@ -28,11 +32,57 @@ gulp.task('copy', function() {
         .on('error', gutil.log);
 });
 
+// 壓縮 HTML
+gulp.task('minify-html', function() {
+    return gulp.src(srcFolder + 'popup.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest(buildFolder))
+        .on('error', gutil.log);
+});
+
+// 壓縮 JS
+gulp.task('minify-js', function() {
+    return gulp.src(srcFolder + '*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest(buildFolder))
+        .on('error', gutil.log);
+});
+
+// 壓縮 CSS
+gulp.task('minify-css', function() {
+    return gulp.src(srcFolder + '*.css')
+        .pipe(minicss())
+        .pipe(gulp.dest(buildFolder))
+        .on('error', gutil.log);
+});
+
+// 壓縮 JSON
+gulp.task('minify-json', function() {
+    return gulp.src(srcFolder + 'manifest.json')
+        .pipe(minijson())
+        .pipe(gulp.dest(buildFolder))
+        .on('error', gutil.log);
+});
+
+// 監控 JS
+gulp.task('watch', function(file) {
+    gulp.watch(srcFolder + '*.js', ['minify-js']);
+    gulp.watch(srcFolder + '*.css', ['minify-css']);
+    gulp.watch(srcFolder + '*.html', ['minify-html']);
+});
+
 // 佈署
 gulp.task('deploy', function() {
     runSequence(
         'clean',
         'copy',
+        'minify-html',
+        'minify-js',
+        'minify-css',
+        'minify-json',
         function() {
             gutil.log('build completed!');
         }
